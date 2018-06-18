4130d9a4dc07371f6d681b1e51c27184102fc78c
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-18 10:16:50 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,7 +21,7 @@ def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times.Times do 
-    phrase
+    puts phrase
   end
 end
 
